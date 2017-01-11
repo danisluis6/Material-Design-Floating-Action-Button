54696a7d4207eccea55cf04606fb9d996c4096cf
danisluis6 <vuongluis@hotmail.com>
2017-01-11 15:50:03 +0700

    ok

--- FloatingActionButton/app/src/main/java/com/example/enclaveit/floatingactionbutton/MainActivity.java
+++ FloatingActionButton/app/src/main/java/com/example/enclaveit/floatingactionbutton/MainActivity.java
@@ -6,44 +6,24 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton fab;
-    private ListView listToDoTask;
     private Toolbar toolbar;
-
-
-    private List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addControlsForNullOfPointer();
-        if(createValueForTesting()){
-                listToDoTask.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,list));
-        };
-    }
-
-    private boolean createValueForTesting() {
-        boolean valid = true;
-        list = new ArrayList<String>();
-        for(int index = 0;index < 100; index++){
-            list.add("Recycle View");
-        }
-        return valid;
     }
 
     private void addControlsForNullOfPointer() {
         fab = (FloatingActionButton)this.findViewById(R.id.fab);
-        listToDoTask = (ListView)this.findViewById(R.id.listToDo);
 
         toolbar = (Toolbar)this.findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_navigation);
