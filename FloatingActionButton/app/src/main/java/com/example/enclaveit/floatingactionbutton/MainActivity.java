3178e29e11d2f3ef6499c88931d6d2ef3acd8103
danisluis6 <vuongluis@hotmail.com>
2017-01-11 14:38:23 +0700

    ok

--- FloatingActionButton/app/src/main/java/com/example/enclaveit/floatingactionbutton/MainActivity.java
+++ FloatingActionButton/app/src/main/java/com/example/enclaveit/floatingactionbutton/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.enclaveit.floatingactionbutton;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton fab;
     private ListView listToDoTask;
+    private Toolbar toolbar;
+
 
     private List<String> list;
 
@@ -21,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addControlsForNullOfPointer();
-
         if(createValueForTesting()){
                 listToDoTask.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,list));
         };
@@ -39,5 +44,21 @@ public class MainActivity extends AppCompatActivity {
     private void addControlsForNullOfPointer() {
         fab = (FloatingActionButton)this.findViewById(R.id.fab);
         listToDoTask = (ListView)this.findViewById(R.id.listToDo);
+
+        toolbar = (Toolbar)this.findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_navigation);
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Test OnClick Event",Toast.LENGTH_LONG).show();
+            }
+        });
+        this.setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        return true;
     }
 }
--- README.md
+++ README.md
@@ -1,30 +1,16 @@
 # MATERIAL DESIGN FLOATING ACTION BUTTON [![Build Status](https://travis-ci.org/nomensa/jquery.hide-show.svg)](https://travis-ci.org/nomensa/jquery.hide-show.svg?branch=master)
 
-##Version1
+##Version2
 
-    - Reset position of Float Action Button. So We need to move to it to anywhere what I like.
-	<android.support.design.widget.FloatingActionButton
-		android:id="@+id/fab"
-		app:fabSize="normal"
-		app:backgroundTint="@color/backgroundTintColor"
-		android:layout_height="wrap_content"
-		android:layout_width="wrap_content"
-		android:src="@drawable/ic_mail"
-		android:layout_margin="@dimen/fab_margin"/>
-
-    - Let's start to custom position of FloatActionButton: You only need to two statements below:
-        app:layout_anchor="@id/listToDo"
-        app:layout_anchorGravity="bottom|center|end"
-
-    - DIFFICULT LEVEL 1: Create Behavior for FloatActionButton
-    + FloatActionButton - Button
-    + app:layout_behavior="" - onClick=""
-    => Create file Java and insert into it.
-    => PROBLEM WITH YOU: I CANN'T SEE ANY ANIMATION AROUND SCROLLING LISTVIEW.
-
-
+    Embedding FloatingActionButton in Header
+    - I intend to custom the position of FloatActionButton. It's the same previous code.
+    - I intend about writting basic sentence and reset position of root. After coding.
 
 <p align="center">
-  <img src="https://github.com/danisluis6/Material-Design-Floating-Action-Button/blob/version1/1.png">
+  <img src="https://github.com/danisluis6/Material-Design-Floating-Action-Button/blob/version2/1.png">
+</p>
+
+<p align="center">
+  <img src="https://github.com/danisluis6/Material-Design-Floating-Action-Button/blob/version2/2.png">
 </p>
 
